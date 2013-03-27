d12355b767d217e870edba9e6401415b7e83599d
Philippe CHARRIERE <ph.charriere@gmail.com>
2013-03-27 16:21:54 +0100

    ...

---
+++ js/data.js
@@ -0,0 +1,51 @@
+window.nanoGolo = {
+    DSL : [
+        {
+            name        : "Glloq",
+            author      : "k33g",
+            description : "Little DSL for little kids",
+            url         : "https://gist.github.com/k33g/5119019",
+            tags        : ["dsl","kids"]
+        }
+    ],
+    TDD : [
+        {
+            name        : "FooBarGolo",
+            author      : "dgageot",
+            description : "Test driving Golo language the FooBarQix way => FooBarGolo !",
+            url         : "https://gist.github.com/dgageot/9895cbae5fbd70892d0d",
+            tags        : ["tests"]
+        },
+        {
+            name        : "Elmira",
+            author      : "k33g",
+            description : "Test-driven development with Golo",
+            url         : "https://gist.github.com/k33g/5229460",
+            tags        : ["tests","dsl"]
+        }
+
+    ],
+    CONSOLE : [
+        {
+            name        : "Rainbow",
+            author      : "k33g",
+            description : "Colors in the terminal",
+            url         : "https://gist.github.com/k33g/5231688",
+            tags        : ["console","terminal","println"]
+        }
+    ],
+    "WEB FRAMEWORKS" : [
+        {
+            name        : "Fast!>>forward",
+            author      : "k33g",
+            description : "Web application server with Redis database 4 Single Page Applications ... or more",
+            url         : "https://github.com/k33g/fastforward",
+            tags        : ["web", "server", "nosql"]
+        }
+    ]
+}
+
+
+
+
+
